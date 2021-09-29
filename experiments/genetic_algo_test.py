7b51c1d56009f5b17b203d37a80aa6d1d11474f9
Kasper Schnack <kschnack@gmail.com>
2021-09-29 22:05:58 +0200

    master: finding max z of -x^2-y^2 as a test case for GA  - failing quite miserably :/

---
+++ experiments/genetic_algo_test.py
@@ -0,0 +1,35 @@
+import math
+import random
+
+import pandas as pd
+
+from commons.genetic_algo import evolve_new_generation
+
+
+def generate_initial_population():
+    for _ in range(7):
+        a = random.randint(0, 100)
+        b = random.randint(0, 100)
+        result = second_order_polynomial_fitness(a, b)
+        print(str(a) + "," + str(b) + "," + str(result))
+
+
+def second_order_polynomial_fitness(a: float, b: float):
+    return (
+        -((a - 40) ** 2) - (b - 40) ** 2
+    )  # taking the sqroot of a negative number complicates things a bit so imma assume we're not gonna choose numbers higher than a=400, b=400 and just let the rest be positive
+
+
+if __name__ == "__main__":
+    prev_pop = pd.read_csv("experiments/sop_initial_population.csv")
+    for i in range(10):
+        print("GENERATION", i)
+        new_pop = evolve_new_generation(prev_pop)
+        new_pop["fitness"] = new_pop.apply(
+            lambda x: second_order_polynomial_fitness(
+                x["initial_jump_distance"], x["score_to_speed_ratio"]
+            ),
+            axis=1,
+        )
+        # print(new_pop)
+        prev_pop = new_pop.copy()
